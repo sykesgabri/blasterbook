863237f7dc26a9157fd4c53f8589349f3794e5cc
Gabriel Sykes <48891219+sykesgabri@users.noreply.github.com>
2022-04-03 12:42:20 +0100

    Add files via upload

---
+++ scripts/js.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
+        document.getElementById("topButt").style.display = "block";
+    }else{
+      document.getElementById("topButt").style.display = "none";
+    }
+}
+
+function topButt(){
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
